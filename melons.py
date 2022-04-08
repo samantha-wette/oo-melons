66e39118c5e6df3593342246207d2f6d394626d0
Samantha Wette <samantha.wette@gmail.com>
2022-04-08 09:27:28 -0700

    International orders fixed

--- melons.py
+++ melons.py
@@ -9,8 +9,9 @@ class AbstractMelonOrder:
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = order_type
         self.tax = tax
+        self.order_type = order_type
+    
     
     def get_total(self):
         """Calculate price, including tax."""
@@ -31,41 +32,26 @@ class DomesticMelonOrder(AbstractMelonOrder):
 
     def __init__(self, species, qty):
         """Initialize domestic melon order attributes."""
-        super().__init__(species, qty, 'domestic', .08)
-       #order_type = "domestic"
-        #self.species = species
-        #self.qty = qty
-        #self.shipped = False
-        #self.order_type = "domestic"
-        #self.tax = 0.08
 
-class InternationalMelonOrder:
+        super().__init__(species, qty, 'domestic', .08)
+
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize international melon order attributes."""
-
-        self.species = species
-        self.qty = qty
+        super().__init__(species, qty, 'international', 0.17)
         self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
 
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
+        #self.species = species
+        #self.qty = qty
+        #self.country_code = country_code
+        #self.shipped = False
+        #self.order_type = "international"
+        #self.tax = 0.17
     def get_country_code(self):
         """Return the country code."""
-
+        #self.country_code()
+    
         return self.country_code
+
